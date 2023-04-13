b125c6477e20bd4e25ebf659d0b79569d009a86e
Zori <46168195+zorislav@users.noreply.github.com>
2023-04-13 10:51:20 +0200

    Add files via upload

---
+++ JS/Vjezba 9 - Nizovi/zavrsno.js
@@ -0,0 +1,21 @@
+//1.
+const names = ['John', 'Jane', 'Bob',, 'Mike'];
+
+for (element of names) {
+    console.log(element);
+}
+
+//2.
+names.push('Zorislav');
+
+//3.
+for (element of names) {
+    if (element === 'Jane') break;
+}
+
+//4.
+for (let i=0; i <= names.length -1; i++) {
+    if (!names[i]) names.splice(i, 1);
+}
+
+console.log(names);
