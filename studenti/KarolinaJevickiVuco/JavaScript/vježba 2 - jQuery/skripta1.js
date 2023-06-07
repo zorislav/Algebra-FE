056990f1ff9d6f6ea603f494aeb92cd299a47fe0
KarolinaJevickiVuco <127890623+KarolinaJevickiVuco@users.noreply.github.com>
2023-06-07 21:12:07 +0200

    JS - osnove programiranja
    JS - osnove programiranja vježbe 2-5

---
+++ studenti/KarolinaJevickiVuco/JavaScript/vježba 2 - jQuery/skripta1.js
@@ -0,0 +1,12 @@
+ 
+
+$(document).ready(function () {
+   $("button").click(function (){
+      $("p").hide();
+   });
+});
+
+
+/*$(document) - pristupamo dokumentu 
+ready - metoda koja u sebi ima funkciju (=što će se dogoditi kad se dokument učita)
+*/
