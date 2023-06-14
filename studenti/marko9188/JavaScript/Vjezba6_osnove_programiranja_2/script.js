b334c6953f07b3f10a66aea3f8f31229b27f67ef
Marko Budimir <marko9188@gmail.com>
2023-06-14 22:13:19 +0200

    Zadaća
    Ne radi

---
+++ studenti/marko9188/JavaScript/Vjezba6_osnove_programiranja_2/script.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+
+function hasUniqueCharacters(password) {
+  const characters = password.split('');
+  const uniqueCharacters = _.uniq(characters);
+
+  return characters.length === uniqueCharacters.length;
+}
+
+function hasOnlyDigits(password) {
+    const digits = password.split('');
+  
+    return _.every(digits, _.isInteger);
+  }
+
+function truncatePassword(password) {
+    return _.truncate(password, { length: 10 });
+  }
+
+  const password = 'pprimjer123';
+
+  console.log(hasUniqueCharacters(password)); // false
+  console.log(hasOnlyDigits(password)); // false
+  console.log(truncatePassword(password)); // pprimjer12
