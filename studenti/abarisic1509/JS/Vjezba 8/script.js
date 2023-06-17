75fe3a2064e44c8b43a5242fb4cd6b39b84657c0
abarisic1509 <abarisic@protonmail.com>
2023-06-17 15:10:16 +0200

    update

--- studenti/abarisic1509/JS/Vjezba 8/script.js
+++ studenti/abarisic1509/JS/Vjezba 8/script.js
@@ -1,18 +1,37 @@
-const password = "abc123456789";
+const password = "abc123";
 
 function chechUniqueChars(string) {
 	const uniqueChars = _.uniq(string.split(""));
 	return uniqueChars.length === string.length;
 }
 
-function checkNumericChars(password) {
-	return /^\d+$/.test(password);
+function chechUniqueChars2(string) {
+	return new Set(string).size === string.length; //set cannot have duplicate values
 }
 
-function truncatePassword(password) {
-	return _.truncate(password, { length: 10, omission: "" });
+function checkNumericChars(string) {
+	return /^\d+$/.test(string);
+}
+function checkNumericChars2(string) {
+	for (let i = 0; i < string.length; i++) {
+		if (string.charCodeAt(i) < 48 || string.charCodeAt(i) > 57) {
+			return false;
+		}
+	}
+	return true;
 }
 
-console.log(chechUniqueChars(password));
-console.log(checkNumericChars(password));
-console.log(truncatePassword(password));
+function truncatePassword(string) {
+	return _.truncate(string, { length: 10, omission: "" });
+}
+function truncatePassword2(string) {
+	return string.slice(0, 10);
+}
+
+console.log("Unique1", chechUniqueChars(password));
+console.log("Unique2", chechUniqueChars2(password));
+console.log("Numeric1", checkNumericChars(password));
+console.log("Numeric2", checkNumericChars2(password));
+console.log("Trim1", truncatePassword(password));
+console.log("Trim2", truncatePassword2(password));
+console.log(new Set([2, 5, 8, 7, 5])); //doesn't work with array of objects
