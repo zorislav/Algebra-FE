4e66b2598506b21b8a8de18f0d7bc0a204268aac
Marko Budimir <marko9188@gmail.com>
2023-06-20 23:44:19 +0200

    do zadaće

---
+++ studenti/marko9188/JavaScript/Vjezba10_promjena_sadrzaja_i_atributa/zadatak/skript.js
@@ -0,0 +1,6 @@
+//1. U folderu /Pocetno se nalaze html i js file kao pocetna tocka.
+// 2. HTML index.html-a treba promijeniti pomocu JS i HTML DOM API-ja tako da:
+// - h1 element sadrži tekst "Karlovac"
+// - span u description div-u je zamijenjen h3 elementom sa tekstom "5 rijeka" 
+// - div s klasom "info" sadrži text "Peta rijeka je piva."
+// - lista u footer elementu izlistava svih 5 karlovačkih rijeka u li elementima
