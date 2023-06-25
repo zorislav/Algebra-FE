d98803c720d6a0a7aae419e17a29de382a47605e
Marko Budimir <marko9188@gmail.com>
2023-06-25 17:01:19 +0200

    Zadaća - promjena sadržaja i atributa

--- studenti/marko9188/JavaScript/Vjezba10_promjena_sadrzaja_i_atributa/zadatak/skript.js
+++ studenti/marko9188/JavaScript/Vjezba10_promjena_sadrzaja_i_atributa/zadatak/skript.js
@@ -4,3 +4,22 @@
 // - span u description div-u je zamijenjen h3 elementom sa tekstom "5 rijeka" 
 // - div s klasom "info" sadrži text "Peta rijeka je piva."
 // - lista u footer elementu izlistava svih 5 karlovačkih rijeka u li elementima
+
+// Zamijeni span element u description div-u s h3 elementom koji sadrži tekst "5 rijeka"
+var spanElement = document.querySelector(".description span");
+var h3Element = document.createElement("h3");
+h3Element.textContent = "5 rijeka";
+spanElement.parentNode.replaceChild(h3Element, spanElement);
+
+// Postavi tekst u div-u s klasom "info" na "Peta rijeka je piva."
+var infoDiv = document.querySelector(".info");
+infoDiv.textContent = "Peta rijeka je piva.";
+
+// Dohvati postojeće li elemente iz liste
+var footerList = document.querySelectorAll("#footer ul li");
+var rivers = ["Kupa", "Mrežnica", "Korana", "Dobra", "Piva"];
+
+// Dodaj tekst rijeke na postojeće li elemente
+footerList.forEach(function(liElement, index) {
+  liElement.textContent = rivers[index];
+});
