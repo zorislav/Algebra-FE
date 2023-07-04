e0d163fd5ffe2396eff6070b8acdf20f1d8292e8
ana-brtan <ana.brtan3@gmail.com>
2023-07-04 23:12:51 +0200

    TODO 1

--- JS/Vjezba 15 - Promjena sadrzaja i atributa/script.js
+++ JS/Vjezba 15 - Promjena sadrzaja i atributa/script.js
@@ -1,14 +1,19 @@
 var h1 = document.querySelector('h1');
-var info = document.querySelector('.info');
-var footer = document.querySelector('footer');
+h1.innerText = 'Kalovac';
+
+
+
 var description = document.querySelector('.description');
+description.innerHTML = 'Grad na <h3>5 rijeka</h3>.';
+
+var info = document.querySelector('.info');
+info.innerText = 'Peta je piva.';
 
 var rivers = ['Mreznica', 'Korana', 'Kupa', 'Dobra', 'Karlovačko'];
 
-h1.innerText = 'Kalovac';
-description.innerHTML = 'Grad na <h3>5 rijeka</h3>.';
-info.innerText = 'Peta je piva.';
 
+
+var footer = document.querySelector('footer');
 for (var i = 0; i < 5; i++) {
 	footer.querySelectorAll('li')[i].innerText = rivers[i];
 }
