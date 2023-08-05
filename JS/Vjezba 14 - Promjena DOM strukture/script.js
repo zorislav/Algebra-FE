f418b81e3d432830b74380e69f5ba7c0e5542b2d
Marko Budimir <127989177+marko9188@users.noreply.github.com>
2023-08-05 21:46:51 +0200

    vjezba

--- JS/Vjezba 14 - Promjena DOM strukture/script.js
+++ JS/Vjezba 14 - Promjena DOM strukture/script.js
@@ -1,12 +1,11 @@
 var descriptionNode = document.querySelector('.description')
-var h1 = document.createElement('h1');
-descriptionNode.parentNode.prepend(h1);
+descriptionNode.parentNode.prepend(document.createElement('h1'));
 
 var infoDiv = document.createElement('div');
 infoDiv.classList.add('info');
 descriptionNode.parentNode.insertBefore(infoDiv, descriptionNode.nextSibling);
 
-var ul = document.getElementById('men');
+var ul = document.getElementById('footer');
 for (var i = 0; i < 3; i++) {
 	ul.appendChild(document.createElement('li'));
 }
