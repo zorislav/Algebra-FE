6585a204fdd1ecd0b3f31bb4f4c80beadd4af8e0
Zori <46168195+zorislav@users.noreply.github.com>
2023-08-05 18:49:52 +0200

    Add files via upload

---
+++ React/Vjezba 4 - indexjs/src/App.js
@@ -0,0 +1,14 @@
+import  { Komponenta1, Komponenta2, Komponenta3, Komponenta4 } from './components/index';
+
+function App() {
+  return (
+    <div>
+      <Komponenta1 />
+      <Komponenta2 />
+      <Komponenta3 />
+      <Komponenta4 />
+    </div>
+  );
+}
+
+export default App;
