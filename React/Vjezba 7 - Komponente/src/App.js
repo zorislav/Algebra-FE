fc91bd7f867bee32e01905482bbc6057e6727f3a
Zori <46168195+zorislav@users.noreply.github.com>
2023-08-06 11:36:00 +0200

    Add files via upload

---
+++ React/Vjezba 7 - Komponente/src/App.js
@@ -0,0 +1,13 @@
+import WelcomeFunction from './WelcomeFunction';
+import WelcomeClass from './WelcomeClass';
+
+function App() {
+  return (
+    <div>
+      <WelcomeFunction />
+      <WelcomeClass />
+    </div>
+  );
+}
+
+export default App;
