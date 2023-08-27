e09235af945342b4980afc30163b651204f113c5
scutvaric <112493123+scutvaric@users.noreply.github.com>
2023-08-27 22:25:40 +0200

    Nedostale vjezbe + JS Parcijalni ispit

---
+++ studenti/Scutvaric/JS/Vjezba 30_parcijalni/src/data.js
@@ -0,0 +1,19 @@
+export const fetchData = async (inputElValue) => {
+    
+    const url = `https://itunes.apple.com/search?term=${inputElValue}&media=music`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const returnData = data.results.map(result => {
+            return {
+                artist: result.artistName,
+                song: result.trackName
+            }
+        });
+
+        return returnData;
+    } catch(error) {
+        return [];
+    }
+}
