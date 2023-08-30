203562fc208cb8728fba4da4a8e3f72f912f3f53
Marko Budimir <marko9188@gmail.com>
2023-08-30 20:52:32 +0200

    react

---
+++ studenti/marko9188/React/Vjezba1/moja-prva-aplikacija/src/App.js
@@ -0,0 +1,12 @@
+import Komponenta1 from "./components/Komponenta1";
+
+function App() {
+  return (
+    <div>
+      <p>moja prva react aplikacija</p>
+      <Komponenta1 />
+    </div>
+  );
+}
+
+export default App;
