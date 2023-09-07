44cef5c67311a3ae91622c74056603c170709041
Marko Budimir <marko9188@gmail.com>
2023-09-07 21:02:00 +0200

    +

---
+++ studenti/marko9188/React/Vjezba8/props/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <p>App</p>
+      </>
+    )
+  }
+}
+
+export default App;
