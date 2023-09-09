9f23d5a5cdf0e242fb82767a86d2878307ddc12c
Marko Budimir <marko9188@gmail.com>
2023-09-09 13:04:59 +0200

    vjezba 8

--- studenti/marko9188/React/Vjezba8/props/src/App.js
+++ studenti/marko9188/React/Vjezba8/props/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+// import UserFunction from './components/UserFunction';
 import './App.css';
+// import UserClass from './components/UserClass';
+import {UserFunction, UserClass, UserChildren} from './components';
+
+
+const users = [
+  {name: 'Ivan', years:30},{name:'Marko', years:35},{name:'Ana', years:25}
+];
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <p>App</p>
+        <h1>React Aplikacija</h1>
+        <p>Ovo stvarno radi</p>
+        <UserFunction name={users[0].name} years={users[0].years} />
+        <UserClass name={users[1].name} years={users[1].years} />
+        <UserChildren name={users[2].name} years={users[2].years}>A hobi mi je plivanje </UserChildren>
       </>
     )
   }
