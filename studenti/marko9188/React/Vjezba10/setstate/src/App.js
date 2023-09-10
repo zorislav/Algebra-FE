479b1b7235e3bf40d4ee073eb9f4b17dc9fb1886
Marko Budimir <marko9188@gmail.com>
2023-09-10 19:29:04 +0200

    vjezba 11 +

--- studenti/marko9188/React/Vjezba10/setstate/src/App.js
+++ studenti/marko9188/React/Vjezba10/setstate/src/App.js
@@ -1,101 +1,67 @@
-// import React from 'react';
-// // import UserFunction from './components/UserFunction';
-// import './App.css';
-// // import UserClass from './components/UserClass';
-// import {UserFunction, UserClass, UserChildren} from './components';
-
-
-// // class App extends React.Component {
-
-// //   constructor(props){
-// //     super(props);
-// //     this.state = {
-// //       users: [{name: 'Ivan', years:30},{name:'Marko', years:35},{name:'Ana', years:25}],
-// //       childrenText: 'A hobi mi je plivanje.'
-// //     };
-
-// //     this.btnClickHandler1 = this.btnClickHandler1.bind(this);
-// //   }
-
-//    state = {
-//    users: [{name: 'Ivan', years:30},{name:'Marko', years:35},{name:'Ana', years:25}],
-//    childrenText: 'A hobi mi je plivanje.'
-//   };
-
-//   btnClickHandler = () => {
-//     console.log('Kliknuo sam');
-//     const newUsers = this.state.users.map((user) => {
-//       return {...user, years: user.years +1};
-//     });
-//     this.setState({users: newUsers});
-//   }
-
-//   // btnClickHandler1 = () => {
-//   //   console.log('Kliknuo sam');
-//   //   const newUsers = this.state.users.map(user => {
-//   //     return {...user, years: user.years -1};
-//   //   });
-//   //   this.setState({users: newUsers});
-//   // }
-
-
-//   render() {
-
-//     const { users } = this.state;
-
-//     return(
-//       <>
-//         <h1>React aplikacija</h1>
-//         <p>Ovo zbilja radi</p>
-//         <UserFunction name={this.state.users[0].name} years={this.state.users[0].years} />
-//         <UserClass name={users[1].name} years={users[1].years} />
-//         <UserChildren name={users[2].name} years={users[2].years}>{this.state.childrenText}</UserChildren>
-//         <button onClick={this.btnClickHandler}>Promjena godina</button>
-//       </>
-//     );
-//   }
-// }
-
-
-// export default App;
-
-
 import React from 'react';
-import { UserFunction, UserChildren, UserClass } from './components';
+// import UserFunction from './components/UserFunction';
+import './App.css';
+// import UserClass from './components/UserClass';
+import {UserFunction, UserClass, UserChildren} from './components';
+
+
+const users = [
+  {name: 'Ivan', years:30},{name:'Marko', years:35},{name:'Ana', years:25}
+];
 
 class App extends React.Component {
 
-  state = {
-    users: [
-      {name: 'Ivan', years: 30},
-      {name: 'Marko', years: 35},
-      {name: 'Ana', years: 25}
-    ],
-    childrenText: "A hobi mi je skijanje"
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     users: [{name: 'Ivan', years:30},{name:'Marko', years:35},{name:'Ana', years:25}],
+  //     childrenText: 'A hobi mi je plivanje.'
+  //   };
+  //   this.btnClickHandler1 = this.btnClickHandler1.bind(this);
+  // }
+
+   state = {
+   users: [{name: 'Ivan', years:30},{name:'Marko', years:35},{name:'Ana', years:25}],
+   childrenText: 'A hobi mi je plivanje.'
   };
 
-  btnClickHandler = () => {
-    const newUsers = this.state.users.map((user) => {
-      return {...user, years: user.years+1};
-    });
-    this.setState({users: newUsers});
-  }
+    btnClickHandler = () => {
+      console.log('klinuo sam');
+      const newUsers = this.state.users.map(user => {
+        return {
+          ...user, years: user.years +1
+        };
+      });
+      this.setState({users: newUsers})
+    }
+
+    // btnClickHandler1 () {
+    //   console.log('klinuo sam');
+    //   const newUsers = this.state.users.map(user => {
+    //     return {
+    //       ...user, years: user.years +1
+    //     };
+    //   });
+    //   this.setState({users: newUsers})
+    // }
 
 
   render() {
 
-    const { users } = this.state;
+    const {users, childrenText} = this.state;
 
-    return(
+    return (
       <>
-        <h1>React aplikacija</h1>
-        <p>Ovo zbilja radi</p>
-        <UserFunction name={this.state.users[0].name} years={this.state.users[0].years} />
+        <h1>React Aplikacija</h1>
+        <p>Ovo stvarno radi</p>
+        <UserFunction name={users[0].name} years={users[0].years} />
         <UserClass name={users[1].name} years={users[1].years} />
-        <UserChildren name={users[2].name} years={users[2].years}>{this.state.childrenText}</UserChildren>
+        <UserChildren name={users[2].name} years={users[2].years}>{childrenText}</UserChildren>
         <button onClick={this.btnClickHandler}>Promjena godina</button>
+        {/* <button onClick={this.btnClickHandler1.bind(this)}>Promjena godina</button>
+        <button onClick={()=>{this.btnClickHandler1()}}>Promjena godina2</button> */}
       </>
-    );
+    )
   }
 }
 
