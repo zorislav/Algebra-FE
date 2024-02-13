e3294de01ad307a0f0d11da78f98f0150420d68d
Zori <46168195+zorislav@users.noreply.github.com>
2024-02-13 10:30:10 +0100

    Delete studenti/abarisic1509 directory

--- studenti/abarisic1509/React/parcijalni/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from "./Button";
-export { default as Loader } from "./Loader";
-export { default as RepoItem } from "./RepoItem";
--- studenti/abarisic1509/React/parcijalni/src/lib/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserReducer from "./slices/userSlice";
-
-export default configureStore({
-	reducer: {
-		user: UserReducer,
-	},
-});
