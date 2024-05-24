eb4e9640e22363c947e299abf4143cb5a6d039d6
matildaskr <160533137+matildaskr@users.noreply.github.com>
2024-05-24 23:16:12 +0200

    20. i 21. 5.

---
+++ studenti/matilda/js/vjezba21/first.js
@@ -0,0 +1,18 @@
+/* 1. Napišite modul za brojanje broja koraka. Modul će se brinuti o povećanju broja koraka, a izvana će se moći pristupiti dvjema metodama modula: walk() i getTotalSteps().
+
+2. Exportajte modul iz datoteke. */
+
+export var Exercise = (function () {
+  var steps = 0;
+  function increaseSteps() {
+    steps++;
+  }
+  return {
+    getTotalSteps: function () {
+      return steps;
+    },
+    walk: function () {
+      increaseSteps();
+    },
+  };
+})();
