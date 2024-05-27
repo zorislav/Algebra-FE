77dbcc5073eea1afcdc2a480fe707ddcb3204d9d
matildaskr <160533137+matildaskr@users.noreply.github.com>
2024-05-27 17:56:36 +0200

    23.5.

---
+++ studenti/matilda/js/vjezba23/script.js
@@ -0,0 +1,17 @@
+/* 1. Napišite Promise koji čeka funkciju getX, i na resolved reagira sa console.log-om vrijednosti vraćene vrijednosti, a na reject reagira sa console.log-om stringa "Oh no!"
+
+2. Ako već niste, prepišite sve funkcije u arrow funkcije. */
+
+const getX = () => Math.random() >= 0.5;
+const prom = new Promise((resolve, reject) => {
+  const x = getX();
+
+  if (x) {
+    return setTimeout(resolve, 1000);
+  } else {
+    return setTimeout(reject, 1000);
+  }
+});
+prom
+  .then(() => console.log("Yay! ${value}"))
+  .catch(() => console.log("Oh,no!"));
