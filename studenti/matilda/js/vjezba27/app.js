4aabf94629d665585f96c7fcc59bc56e4134ec9a
matildaskr <160533137+matildaskr@users.noreply.github.com>
2024-06-06 15:30:40 +0200

    4.6.

--- studenti/matilda/js/vjezba27/app.js
+++ studenti/matilda/js/vjezba27/app.js
@@ -1,11 +1,11 @@
-/*jsLint devel single */
+/*jsLint white: true devel single */
 
 const name = "James";
 
 const person = { first: "name1" };
-
 console.log(person);
 
 const sayHelloLinting = function (fName) {
-  console.log(`Hello linting, ${fName}`);
+  console.log(fName);
+  /*  console.log(`Hello linting, ${fName} */
 };
