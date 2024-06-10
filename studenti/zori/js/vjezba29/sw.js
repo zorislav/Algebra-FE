b79b6ab2d2919524bec4facecdf88bc2ec129cf6
Zorislav <160596348+ivanhorvat1171@users.noreply.github.com>
2024-06-10 21:11:27 +0200

    nakon 10.6.

--- studenti/zori/js/vjezba29/sw.js
+++ studenti/zori/js/vjezba29/sw.js
@@ -1,7 +1,9 @@
 self.addEventListener("install", ()=>{
   console.log("Service Worker Installed");
-})
+});
 
 self.addEventListener("fetch", (event)=>{
   console.log("You Fetched " +  event);
-})
+});
+
+
