713df76a034ff35b8a3ab2096abf16cad5e1dfc7
marko0905 <marko0905@gmail.com>
2024-06-13 11:41:08 +0200

    New 13.06.2024.

--- studenti/Marko_Mijacevic/JS/vježba27 - Otkrivanje pogrešaka/app.js
+++ studenti/Marko_Mijacevic/JS/vježba27 - Otkrivanje pogrešaka/app.js
@@ -1,4 +1,4 @@
-/*jslint devel single */
+/*jslint  white:true  devel single */
 
 const name = 'James';
 
@@ -8,6 +8,15 @@ console.log(person);
 
 const sayHelloLinting = function (fName) {
     console.log(`Hello linting, ${fName}`);
+    //console.log(`Hello linting, ${name}`);
 };
 
 //const sayHelloLinting = (fName) => console.log(`Hello linting, ${fName}`);
+
+
+//npm init --yes
+//npm install eslint --save-dev
+
+//npm install -g wslint-cli
+
+//npm install -za opent instalirati node_modules preko package.json
