35ece40fda656e63d5b9f2b9f466904845ae0e36
MatoTenzera <tenzera.mato@gmail.com>
2024-07-10 08:16:48 +0200

    React v6,8,9,10,11,22

---
+++ studenti/MatoTenzera/ReactJS/Vjezba10 - setState/set-state/src/user/UserFunction.jsx
@@ -0,0 +1,8 @@
+function UserFunction ({ime,godine}) {
+
+    //const {ime,godine} = props; 
+
+    return (<p>Pozdrav moje ime je {ime} i imam {godine} godina.</p>);
+}
+
+export default UserFunction;
--- studenti/MatoTenzera/ReactJS/Vjezba11 - useState hook/use-state/src/App.js
+++ studenti/MatoTenzera/ReactJS/Vjezba11 - useState hook/use-state/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import {useState} from 'react';
 import './App.css';
 import { UserFunction,UserClass,UserChildren } from './user';
 
-const users = [
+
+
+function App() {
+
+
+const [users,setUsers] = useState([
   {
     name:"ivan",
     years: 30
@@ -15,22 +20,37 @@ const users = [
     name:"ana",
     years: 25
   }
-];
+]);
 
-class App extends React.Component{
+const [childrenText, setChildrenText] = useState("plivanje");
 
-  render(){
-    return (
+
+  const btnClickHandler = () => {
+
+
+    const newUsers = users.map((user) => {
+      return {...user,years:user.years + 1};
+    })
+
+
+    setUsers(newUsers);
+
+  }
+
+
+  return (
       <div className="App">
         <h1>React App</h1>
         <p>Ovo radi!</p>
         <UserFunction ime={users[0].name} godine={users[0].years}/>
         <UserClass ime={users[1].name} godine={users[1].years}/>
         <UserChildren ime={users[2].name} godine={users[2].years} fontSize="30px">plivanje--ovdje definiramo vrijednost prop od children</UserChildren>
+        <UserChildren godine={users[2].years}> {childrenText} </UserChildren>
+        <button onClick={btnClickHandler}>promjena godina</button>
 
       </div>
     );
-  }
+  
 }
 
 export default App;
