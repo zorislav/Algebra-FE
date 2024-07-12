2c0ea4a262149d2888df2109c6951773d5234ca9
Zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2024-07-12 20:56:34 +0200

    Nakon 12.7.

--- studenti/zori/react/V15_obrasci2/forme2/src/App.js
+++ studenti/zori/react/V15_obrasci2/forme2/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { User, NewUser  } from './user';
+
+class App extends React.Component {
+
+  state = {
+    users: [
+      {
+        id: "1",
+        name: "Ivan",
+        years: 30
+      },
+      {
+        id: "2",
+        name: "Marko",
+        years: 25
+      },
+      {
+        id: "3",
+        name: "Ana",
+        years: 25
+      }
+    ],
+    childrenText: "plivanje"
+  };
+  
+  btnClickHandler = () => {
+
+    const newUsers = this.state.users.map((user) => {
+      return { ...user,  years: user.years + 1 };
+    })
+
+
+    this.setState({ users: newUsers});
+  }
+
+  nameChangeHandler = (event, index) => {
+
+    const { users } = this.state;
+
+    const newUsers = [...users];
+    newUsers[index].name = event.target.value;
+
+    this.setState({ users: newUsers });
+
+  }
+
+  addUserHandler = ({name, age}) =>{
+
+    const {users} = this.state;
+
+    const newUser = {
+      id: `${name}_${new Date().getTime()}`,
+      name: name,
+      years: age
+    }
+
+    const newUsers = [...users, newUser];
+
+    this.setState({ users: newUsers});
+
+  }
+  
+  render() {
+
+    const { users, childrenText } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React Aplikacija</h1>
+        <p>Ovo zaista radi</p>
+        <button onClick={this.btnClickHandler}>Promjena godina</button>
+        {
+          users.map((user, index) => 
+            <User key={user.id}
+              ime={user.name} 
+              godine={user.years} 
+              onNameChange={(event) => this.nameChangeHandler(event, index)}
+            />
+
+          )
+        }
+        <hr/>
+        <NewUser onAddUser={this.addUserHandler} />
+      </div>
+    );
+
+  }
 }
 
 export default App;
+
