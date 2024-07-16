c287b5f2c5ea2ee07367f3540b786c5e529b0c08
Marko Mijacevic <marko0905@gmail.com>
2024-07-16 20:42:12 +0200

    new 16/07/24

---
+++ studenti/Marko_Mijacevic/React/v17-Higher-Order-Components_(HOC)/hoc/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+
+function Button(props){
+  return <button {...props}></button>;
+}
+
+function withColor(Element){
+  return function({ boja, ...otherProps }){
+    return <Element {...otherProps}style={ {backgroundColor: boja} } />
+  }
+}
+
+const ColoredButton = withColor(Button);
+
+function clickHandler(){
+  alert("Kliknuo");
+}
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Button>Moj button</Button>
+      <ColoredButton boja="red">Bok, ja sam crveni</ColoredButton>
+      <ColoredButton boja="yellow">Bok, ja sam žuti</ColoredButton>
+      <ColoredButton boja="green" onClick={clickHandler} >Bok, ja sam zelen</ColoredButton>
+    </div>
+  );
+}
+
+export default App;
