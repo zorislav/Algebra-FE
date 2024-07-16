f16bed5cee79e5272fddbeabfe31301c67b89bc3
Zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2024-07-16 20:41:16 +0200

    Nakon 16.7.

--- studenti/zori/react/V18_router/router/src/App.js
+++ studenti/zori/react/V18_router/router/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import './App.css';
+
+function Index() {
+
+  return (
+    <>
+      <h1>Welcome to Router!</h1>
+      <p>Ovo je primjer korištenja React Router-a</p>
+      <p>Sad sam na početnoj stranici.</p>
+    </>
+  );
+}
+
+function Komponenta1() {
+
+  return (
+    <>
+      <h1>Welcome to Komponenta1</h1>
+      <p>Ovo je primjer korištenja React Router-a</p>
+      <p>Sad sam na Komponenti1</p>
+    </>
+  ); 
+}
+
+function Komponenta2() {
+
+  const navigate =  useNavigate();
+
+  function clickHandler() {
+    navigate("/");
+  }
+
+  return (
+    <>
+      <h1>Welcome to Komponenta2</h1>
+      <p>Ovo je primjer korištenja React Router-a</p>
+      <p>Sad sam na Komponenti2</p>
+      <button onClick={clickHandler}>Home</button>      
+    </>
+  );
+}
+
+function NotFound() {
+  
+  return (
+    <>
+      <h1>Welcome to Router!</h1>
+      <p>Tražena stranica nije pronađena!</p>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul style={{ listStyleType: "none"}}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/k1">Komponenta 1</Link>
+          </li>
+          <li>
+            <Link to="/k2">Komponenta 2</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/k1" element={<Komponenta1/>} />
+        <Route path="/k2" element={<Komponenta2/>}/>
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
     </div>
   );
 }
--- studenti/zori/react/V19_rest/rest/src/index.js
+++ studenti/zori/react/V19_rest/rest/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 root.render(
   <React.StrictMode>
-    <App />
+    <App2 />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
