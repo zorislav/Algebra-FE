968e3add4ecf5ba60155d6fb86b64f64f21f0438
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-10-01 20:58:29 +0200

    Upd 1.10.

---
+++ 8_Studenti/Zorislav/5_React/Bonus/life/src/components/Child1.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+class Child1 extends React.Component {
+
+  UNSAFE_componentWillMount(){
+    console.log("Child1 will mount");
+  }
+
+  componentDidMount(){
+    console.log("Child1 did mount");
+  }
+
+  componentWillUnmount(){
+    console.log("Child1 will unmount");
+  }
+
+  componentDidUpdate(){
+    console.log("Child1 did update");
+  }
+
+  shouldComponentUpdate(){
+
+    // Logika ...
+
+    return true;
+  }
+
+  handleCounter = () => {
+
+    const { uvecaj } = this.props;
+    uvecaj();
+
+  }
+
+  render(){
+
+    const { brojac } = this.props;
+
+    return (
+      <>
+        <h3>Child1</h3>
+        <p>{brojac}</p>
+        <button onClick={this.handleCounter}>Incerase Counter</button>
+        <br />
+      </>
+    );
+  }
+}
+
+export default Child1;
