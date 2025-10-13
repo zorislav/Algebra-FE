12dd45dfd25012468df7ea8c84f0e84f4098caae
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-10-13 20:53:03 +0200

    Upd 13.10.

---
+++ 8_Studenti/Zorislav/5_React/V23_Mobx/mobx-test/src/components/GroceryAdd.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function GroceryAdd(){
+
+  const [grocery, setGrocery] = useState('');
+
+  function groceryChangeHandler(event){
+    setGrocery(event.target.value);
+  }
+
+  function groceryAddHandler(){
+    // Spremi lokalno stanje u globalno stanje
+    setGrocery('');
+  }
+
+  return (
+    <>
+      <label htmlFor="kupi">Kupiti: </label>
+      <input id="kupi" type="text" value={grocery} onChange={groceryChangeHandler}/>
+      <button onClick={groceryAddHandler}>Add</button>
+    </>
+  );
+}
+
+export default GroceryAdd;
+
--- 8_Studenti/Zorislav/5_React/V24_Todo/todo/src/index.js
+++ 8_Studenti/Zorislav/5_React/V24_Todo/todo/src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store/redux-store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  
+    <App />
   </React.StrictMode>
 );
 
