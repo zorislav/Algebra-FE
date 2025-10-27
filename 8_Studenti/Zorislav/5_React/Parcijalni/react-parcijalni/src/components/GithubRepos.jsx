06ba117350a6c55d25388edde105f6a9492ac120
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-10-27 20:50:44 +0100

    Upd 27.10. 2. dio

---
+++ 8_Studenti/Zorislav/5_React/Parcijalni/react-parcijalni/src/components/GithubRepos.jsx
@@ -0,0 +1,17 @@
+function GithubRepos({ repos }){
+
+  return (
+    <div>
+      <p>
+        <strong>REPOSITORIES:</strong>
+      </p>
+      <ul style={{ listStyleType: "none"}}>
+        {
+          repos.map((repo) => <li key={repo.id}>{repo.name}</li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default GithubRepos;
---
+++ 8_Studenti/Zorislav/5_React/Parcijalni/react-parcijalni/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as UserForm } from './UserForm';
+export { default as GithubUser } from './GithubUser';
+export { default as GithubRepos } from './GithubRepos';
